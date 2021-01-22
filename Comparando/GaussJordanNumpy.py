2a865c2dd35278452858e9f618a9993a4fbaf47b
antoniozahe <antoniozamorahz10@gmail.com>
2021-01-22 00:23:18 -0600

    Señor me has mirado a los ojos

---
+++ Comparando/GaussJordanNumpy.py
@@ -0,0 +1,41 @@
+import numpy as np
+from timeit import default_timer as timer
+
+variables = 3 #input("Número de ecuaciones: ")
+
+filas = variables
+columnas = int(variables) + 1
+
+# Inicializamos Matriz en 0
+matriz = np.zeros((10, 10))
+
+# Usuario asigna valore de la matriz
+for i in range(0, int(filas)):
+    for j in range(0, columnas):
+        mensaje = 'Valor elemento [' + str(i) + '][' + str(j) + ']: '
+        matriz[i][j] = input(str(mensaje))
+
+start = timer()
+
+# Row Echelon
+for i in range(0, int(variables)):
+    for j in range(0, int(variables)):
+        if i != j:
+            coc = float(matriz[j][i]) / float(matriz[i][i])
+            for k in range(0, columnas):
+                matriz[j][k] = float(matriz[j][k]) - float(coc) * float(matriz[i][k])
+
+# Igualamos Diagonal a 1
+for i in range(0, int(variables)):
+    div = float(matriz[i][i])
+    for j in range(0, int(columnas)):
+        matriz[i][j] = float(matriz[i][j]) / div
+
+end = timer()
+
+# Imprimimos Soluciones
+print("Solución")
+for i in range(0, int(variables)):
+    print("x" + str(i) + " = " + str(matriz[i][int(variables)]))
+
+print((end - start)*(10**6))
